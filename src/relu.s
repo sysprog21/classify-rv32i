a59aa46c61f3eb8285ad1290b2deaf13029b51cf
Ting <ting@Tings-MacBook-Pro-3.local>
2024-11-16 21:26:58 +0800

    add relu.s

--- src/relu.s
+++ src/relu.s
@@ -28,8 +28,26 @@ relu:
     li t1, 0             
 
 loop_start:
-    # TODO: Add your own implementation
+    bge t1, a1, loop_end      # Check if the loop index has reached the number of elements
+
+    slli t2, t1, 2            # Calculate byte offset (t1 * 4)
+    add t3, a0, t2            # Calculate the address of the current array element
+
+    lw t4, 0(t3)              # Load the current element into t4
+    blez t4, set_zero         # If t4 <= 0, set it to 0
+    j skip_set_zero           # Otherwise, skip to the next iteration
+
+set_zero:
+    li t4, 0                  # Set the current element to 0
+    sw t4, 0(t3)              # Store the modified value back to the array
+
+skip_set_zero:
+    addi t1, t1, 1            # Increment loop index
+    j loop_start              # Repeat loop
+
+loop_end:
+    jr ra                     # Return from the function
 
 error:
-    li a0, 36          
-    j exit          
+    li a0, 36                 # Load error code 36
+    j exit                    # Terminate program with error code
